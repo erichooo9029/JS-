6ee1a8abaf4867aa301c6755a23ce89c2d1abdb9
erichooo9029 <71763399+erichooo9029@users.noreply.github.com>
2023-02-20 20:06:18 +0800

    Add files via upload

---
+++ cardid.js
@@ -0,0 +1,401 @@
+var card_pool = [
+	{"id": 0,
+	"name": "青眼白龍", 
+	 
+	"attack": 3000,  
+	"CardDescription":"以高攻擊力自豪的傳說中的白龍。無論如何都能將對手粉碎，其破壞力不能計算。"
+},
+{
+	"id": 1,
+	"name": "黑魔導", 
+	 
+	"attack": 2500,  
+	"type": [""],
+	"CardDescription":"在魔法使中，攻擊力、守備力都是最高等級。"
+},
+{
+	"id": 2,
+	"name": "真紅眼黑龍", 
+	 
+	"attack": 2400,  
+	"type": [""],
+	"CardDescription":"具有真紅眼的黑龍，憤怒的黑火會燒盡眼中所映現的一切事物。"
+},
+{
+	"id": 3,
+	"name": "黑魔導女孩", 
+	 
+	"attack": 2000,  
+	"type": [""],
+	"CardDescription":"必定給對手1000點額外傷害"
+},
+{
+	"id": 4,
+	"name": "人造人-念力震懾者", 
+	 
+	"attack": 2400,  
+	"type": [""],
+	"CardDescription":"無,然後看起來有點噁心"
+},
+{
+	"id": 5,
+	"name": "靈巧的精靈劍士", 
+	 
+	"attack": 1400,  
+	"type": [""],
+	"CardDescription":"學會了劍術的精靈。以快速的攻擊作弄敵人。"
+},
+{
+	"id": 6,
+	"name": "時間魔術師", 
+	 
+	"attack": 500,  
+	"type": [""],
+	"CardDescription":"對方攻擊力下降500點"
+},
+{
+	"id": 7,
+	"name": "召喚神 艾克索迪亞", 
+	 
+	"attack": 0,  
+	"type": [""],
+	"CardDescription":"抽到此卡時,我方獲得決鬥勝利"
+},
+{
+	"id": 8,
+	"name": "栗子球", 
+	 
+	"attack": 300,  
+	"type": [""],
+	"CardDescription":"主角們常見的王牌之一"
+},
+{
+	"id": 9,
+	"name": "E.Hero 新宇俠", 
+	 
+	"attack": 2500,  
+	"type": [""],
+	"CardDescription":"來自新空間的新E.HERO。與新空間俠接觸融合，發揮出未知的力量！"
+},
+{
+	"id": 10,
+	"name": "幻獸王加賽爾", 
+	 
+	"attack": 1500,  
+	"type": [""],
+	"CardDescription":"奔跑起來的速度太快，身姿看似幻影的野獸。"
+},
+{
+	"id": 11,
+	"name": "暗黑騎士蓋亞", 
+	 
+	"attack": 2300,  
+	"type": [""],
+	"CardDescription":"騎著速度比風還快的馬的騎士。要注意突進攻擊。"
+},
+{
+	"id": 12,
+	"name": "詛咒之龍", 
+	 
+	"attack": 2000,  
+	"type": [""],
+	"CardDescription":"邪惡的龍。使用暗之力的攻擊很強力。"
+},
+{
+	"id": 13,
+	"name": "惡魔的召喚", 
+	 
+	"attack": 2500,  
+	"type": [""],
+	"CardDescription":"使用黑暗力量，迷惑人心的惡魔。在惡魔族中以相當強大的力量著稱。"
+},
+{
+	"id": 14,
+	"name": "磁石戰士α", 
+	 
+	"attack": 1400,  
+	"type": [""],
+	"CardDescription":"用α、β、γ來變形合體。"
+},
+{
+	"id": 15,
+	"name": "咒符龍", 
+	 
+	"attack": 2900,  
+	"type": [""],
+	"CardDescription":"無名之龍 ”「蒂邁歐之眼」和「黑魔術師」融合出來的龍騎士形態。融合後，「"
+},
+{
+	"id": 16,
+	"name": "龍騎士黑魔導女孩", 
+	 
+	"attack": 2600,  
+	"type": [""],
+	"CardDescription":" 無名之龍 ·蒂邁歐”和「黑魔術少女」融合出來的龍騎士形態。"
+},
+{
+	"id": 17,
+	"name": "龍破壞之劍士-巴斯達布雷達", 
+	 
+	"attack": 2800,  
+	"type": [""],
+	"CardDescription":"「擅長抹殺龍族的傳說戰士」。"
+},
+{
+	"id": 18,
+	"name": "太陽神的翼神龍", 
+	 
+	"attack": 0,  
+	"type": [""],
+	"CardDescription":"支付生命值到100。此卡的攻擊力上升支付的數值。"
+},
+{
+	"id": 19,
+	"name": "青眼亞白龍", 
+	 
+	"attack": 3000,  
+	"type": [""],
+	"CardDescription":"青眼白龍的亞種,比青眼白龍多一些能力"
+},
+{
+	"id": 20,
+	"name": "歐西里斯的天空龍", 
+	 
+	"attack": 0,  
+	"type": [""],
+	"CardDescription":"此卡攻擊力為回合數*1000點"
+},
+{
+	"id": 21,
+	"name": "歐貝利斯克的巨神兵", 
+	 
+	"attack": 5000,  
+	"type": [""],
+	"CardDescription":"抽到此卡回復生命值2000點"
+},
+{
+	"id": 22,
+	"name": "守護神官瑪哈特", 
+	 
+	"attack": 2500,  
+	"type": [""],
+	"CardDescription":"前世是法老王的神官和朋友馬哈特，死後化身石板怪獸仍然堅守著信義的承諾。"
+},
+{
+	"id": 23,
+	"name": "深淵眼白龍", 
+	 
+	"attack": 4500,  
+	"type": [""],
+	"CardDescription":"蘊藏於無窮時間之源的純白之力，揚起在靈魂的交錯迴響中震顫的雙翼，從蒼青的深淵中現身吧!淵眼白龍!"
+},
+{
+	"id": 24,
+	"name": "沉默劍士 LV7", 
+	 
+	"attack": 2800,  
+	"type": [""],
+	"CardDescription":"光屬性戰士和魔法使，是遊戲王動畫中，武藤遊戲所持有的牌，特色是對抗魔法的能力。"
+},
+{
+	"id": 25,
+	"name": "星塵龍", 
+	 
+	"attack": 2500,  
+	"type": [""],
+	"CardDescription":"聚集的祈願將成為新生閃光之星。成為光芒照射的道路吧！飛翔吧，星塵龍!"
+},
+{
+	"id": 26,
+	"name": "究極龍騎士", 
+	 
+	"attack": 5000,  
+	"type": [""],
+	"CardDescription":"作為「五神龍」的克星存在，是遊戲王法老王阿圖姆和海馬瀨人的究極融合怪獸。"
+},
+{
+	"id": 27,
+	"name": "青眼混沌MAX龍", 
+	 
+	"attack": 4000,  
+	"type": [""],
+	"CardDescription":"以「混沌型態」降臨。"
+},
+{
+	"id": 28,
+	"name": "炎之劍士", 
+	 
+	"attack": 1800,  
+	"type": [""],
+	"CardDescription":"遊刃有餘地穿梭在火海中發動攻擊 據說是完成了百人斬的傳說中的劍豪。"
+},
+{
+	"id": 29,
+	"name": "青眼究極龍", 
+	 
+	"attack": 4500,  
+	"type": [""],
+	"CardDescription":"攻擊・守備が最高のなかなか手に入らない超レアカード"
+},
+{
+	"id": 30,
+	"name": "護城翼龍", 
+	 
+	"attack": 1400,  
+	"type": [""],
+	"CardDescription":"守護山寨的龍，從空中急速下降攻擊敵人。"
+},
+{
+	"id": 31,
+	"name": "溶岩魔神熔岩石魔", 
+	 
+	"attack": 4000,  
+	"type": [""],
+	"CardDescription":"在強大的攻擊力下,需要支付1000點的生命值"
+},
+{
+	"id": 32,
+	"name": "超魔導劍士斬龍黑魔導", 
+	 
+	"attack": 2900,  
+	"type": [""],
+	"CardDescription":" 強大的魔法壓制能力，就算對手不是龍族，如果自己手牌數量足夠，壓制力也是相當足夠。"
+},
+{
+	"id": 33,
+	"name": "巨大無尾熊", 
+	 
+	"attack": 2700,  
+	"type": [""],
+	"CardDescription":"非常巨大的死亡樹熊的一種。雖然性格溫順。但是因為擁有非常強大的力量而令人感到恐懼。"
+},
+{
+	"id": 34,
+	"name": "黑的魔法神官", 
+	 
+	"attack": 3200,  
+	"type": [""],
+	"CardDescription":"在面對青眼究極龍的時候，由於魔法族的存在可以減少其攻擊力，最後也是一舉殲滅了究極龍"
+},
+{
+	"id": 35,
+	"name": "千年龍", 
+	 
+	"attack": 2400,  
+	"type": [""],
+	"CardDescription":"融合：「時間魔術師」＋「寶貝龍」。"
+},
+{
+	"id": 36,
+	"name": "混沌士兵", 
+	 
+	"attack": 3000,  
+	"type": [""],
+	"CardDescription":"以「混沌的儀式」的效果來儀式召喚。"
+},
+{
+	"id": 37,
+	"name": "漆黑的豹戰士", 
+	 
+	"attack": 2000,  
+	"type": [""],
+	"CardDescription":"Wakanda Forever"
+},
+{
+	"id": 38,
+	"name": "昆蟲女王", 
+	 
+	"attack": 2200,  
+	"type": [""],
+	"CardDescription":"羽蛾の愛牌"
+},
+{
+	"id": 39,
+	"name": "磁石戰士γ", 
+	 
+	"attack": 1500,  
+	"type": [""],
+	"CardDescription":"用α、β、γ來變形合體"
+},
+{
+	"id": 40,
+	"name": "磁石戰士電磁武神", 
+	 
+	"attack": 3500,  
+	"type": [""],
+	"CardDescription":"融合/效果：「磁石戰士α」+「磁石戰士β」+「磁石戰士γ」通過α、β、γ來變形合體。也可以把合體解除。"
+},
+{
+	"id": 41,
+	"name": "暗黑魔族基爾法惡魔", 
+	 
+	"attack": 2200,  
+	"type": [""],
+	"CardDescription":"武藤遊戲牌組裡最強的惡魔族精銳，少見的稀有卡，和上級怪獸『惡魔的召喚』齊名。"
+},
+{
+	"id": 42,
+	"name": "五帝神龍", 
+	 
+	"attack": 5000,  
+	"type": [""],
+	"CardDescription":"龍族怪獸x5 此卡以融合召喚方式特殊召喚。"
+},
+{
+	"id": 43,
+	"name": "混沌魔術師", 
+	 
+	"attack": 2400,  
+	"type": [""],
+	"CardDescription":"看透黑暗與光明,將兩者統一且融合的魔術師"
+},
+{
+	"id": 44,
+	"name": "黑炎騎士", 
+	 
+	"attack": 2200,  
+	"type": [""],
+	"CardDescription":"「黑魔導」+「炎之劍士」。"
+},
+{
+	"id": 45,
+	"name": "混沌黑魔術師", 
+	 
+	"attack": 2800,  
+	"type": [""],
+	"CardDescription":"「黑魔術師」經過「光與暗的洗禮」進化後的形態。"
+},
+{
+	"id": 46,
+	"name": "VWXYZ龍彈砲", 
+	 
+	"attack": 3000,  
+	"type": [""],
+	"CardDescription":"「VW虎式彈射器」+「XYZ龍砲」"
+},
+{
+	"id": 47,
+	"name": "紅惡魔龍", 
+	 
+	"attack": 3000,  
+	"type": [""],
+	"CardDescription":"王只有一個 那就是我！！"
+},
+{
+	"id": 48,
+	"name": "魔法劍士特蘭斯", 
+	 
+	"attack": 2600,  
+	"type": [""],
+	"CardDescription":"實力相當強的奇特魔法師。好像從異空間旅行歸來了。"
+},
+{
+	"id": 49,
+	"name": "蒼炎的劍士", 
+	 
+	"attack": 1800,  
+	"type": [""],
+	"CardDescription":"劍上纏繞著幽藍火焰的強大劍士"
+}
+	
+	]
